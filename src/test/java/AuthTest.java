ac76426da21937d5b08d0972198d57ad69861b0c
Marcelo <khmark04@mail.ru>
2022-04-08 01:12:48 +0300

    allure add

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -2,6 +2,7 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
